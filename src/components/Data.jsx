79526a1968cb4b365974f02c0c7c0d7a89274f84
MD Fahim Patowary <70106181+fahimgithub1@users.noreply.github.com>
2023-02-04 00:47:14 +0600

    Add files via upload

---
+++ src/components/Data.jsx
@@ -0,0 +1,41 @@
+// import { useState } from "react";
+import {
+  useGetAllProductsQuery,
+  useGetProductQuery,
+} from "../features/apiSlice";
+
+export const Data = () => {
+  // const [allData, setAllData] = useState("");
+
+  const {
+    data: allProductsData,
+    isLoading,
+  } = useGetAllProductsQuery();
+  const { data: singleProductData } = useGetProductQuery("iphone");
+
+  console.log(allProductsData);
+  console.log(singleProductData);
+
+
+  if (isLoading) return <h1> Loading...</h1>
+
+
+  return (
+    <div className="row"> 
+      {
+        singleProductData.products.map((item)=>
+         <div className="col-6" id={item.id}>
+          <img src={item.images[0]} alt="" />
+          <title>{item.title}</title>
+          <h3>Price : {item.price}</h3>
+          <p> 
+            <b>Description : </b> 
+            <br /> 
+            {item.description.slice(0,50)}</p>
+         </div>
+        )
+      }
+
+    </div>
+  )
+};
